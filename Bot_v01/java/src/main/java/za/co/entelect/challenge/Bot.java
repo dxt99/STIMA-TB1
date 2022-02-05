d411c00df6d205b9540765e9e23f406e41d31fd5
dxt99 <ericlouis19x@gmail.com>
2022-02-05 13:54:18 +0700

    added v01 folder

---
+++ Bot_v01/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -0,0 +1,85 @@
+package za.co.entelect.challenge;
+
+import za.co.entelect.challenge.command.*;
+import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
+import za.co.entelect.challenge.enums.Terrain;
+
+import java.util.*;
+
+import static java.lang.Math.max;
+
+public class Bot {
+
+    private static final int maxSpeed = 9;
+    private List<Integer> directionList = new ArrayList<>();
+
+    private Random random;
+    private GameState gameState;
+    private Car opponent;
+    private Car myCar;
+    private final static Command FIX = new FixCommand();
+
+    public Bot(Random random, GameState gameState) {
+        this.random = random;
+        this.gameState = gameState;
+        this.myCar = gameState.player;
+        this.opponent = gameState.opponent;
+
+        directionList.add(-1);
+        directionList.add(1);
+    }
+
+    public Command run() {
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
+        if (myCar.damage >= 2) {
+            return new FixCommand();
+        }
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+            return new BoostCommand();
+        }
+        if (hasPowerUp(PowerUps.EMP, myCar.powerups)){
+            return new EmpCommand();
+        }
+        if (hasPowerUp(PowerUps.OIL, myCar.powerups)){
+            return new OilCommand();
+        }
+        if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)){
+            return new LizardCommand();
+        }
+        if (hasPowerUp(PowerUps.TWEET, myCar.powerups)){
+            return new TweetCommand(opponent.position.lane,opponent.position.block+1);
+        }
+        return new AccelerateCommand();
+    }
+
+    /**
+     * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
+     * traversed at max speed.
+     **/
+    private List<Object> getBlocksInFront(int lane, int block) {
+        List<Lane[]> map = gameState.lanes;
+        List<Object> blocks = new ArrayList<>();
+        int startBlock = map.get(0)[0].position.block;
+
+        Lane[] laneList = map.get(lane - 1);
+        for (int i = max(block - startBlock, 0); i <= block - startBlock + Bot.maxSpeed; i++) {
+            if (laneList[i] == null || laneList[i].terrain == Terrain.FINISH) {
+                break;
+            }
+
+            blocks.add(laneList[i].terrain);
+
+        }
+        return blocks;
+    }
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp: available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
